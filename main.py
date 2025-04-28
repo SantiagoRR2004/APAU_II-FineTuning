6a2ceb89833e47cc34ccde15e4f5b6369d24bd97
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-28 10:29:12 +0200

    Percentage function

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import os
 import IOB
 from predict_crf import CRFFeatures
+from typing import List, Tuple
 
-if __name__ == "__main__":
-    currentDir = os.path.dirname(os.path.abspath(__file__))
 
-    originalFile = os.path.join(currentDir, "data", "ner-es.trainOld.csv")
+def printPercentages(sentences: List[List[Tuple[str, str]]]) -> None:
+    """
+    Print the percentage of tokens with and without labels in the given sentences.
 
-    iob = IOB.IOB()
-    feats = CRFFeatures()
+    Args:
+        - sentences (List[List[Tuple[str, str]]]): A list of sentences, where each sentence is a list of tuples.
 
-    sentences = [
-        [tuple(token) for token in sent] for sent in iob.parse_file(originalFile)
-    ]
-
+    Returns:
+        - None
+    """
     withLabel = 0
     withoutLabel = 0
 
-    for i, sentence in enumerate(sentences):
-        for j, token in enumerate(sentence):
+    for _, sentence in enumerate(sentences):
+        for _, token in enumerate(sentence):
             if len(token) > 1:
                 withLabel += 1
             else:
@@ -35,3 +35,16 @@ if __name__ == "__main__":
             withoutLabel / (withLabel + withoutLabel) * 100
         )
     )
+
+
+if __name__ == "__main__":
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+
+    originalFile = os.path.join(currentDir, "data", "ner-es.trainOld.csv")
+
+    iob = IOB.IOB()
+    feats = CRFFeatures()
+
+    sentences = iob.parse_file(originalFile)
+
+    printPercentages(sentences)
