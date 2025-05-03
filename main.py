ab54c2c0cd66a493b07bc14618f4c4d94569003a
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-05-03 18:47:41 +0200

    Save unlabeled

--- main.py
+++ main.py
@@ -37,6 +37,58 @@ def printPercentages(sentences: List[List[Tuple[str, str]]]) -> None:
     )
 
 
+def divideSentences(
+    sentences: List[List[Tuple[str, str]]],
+) -> Tuple[List[List[Tuple[str, str]]], List[List[Tuple[str, str]]]]:
+    """
+    Divide the sentences into two parts based on the given percentage.
+
+    Args:
+        - sentences (List[List[Tuple[str, str]]]): A list of sentences, where each sentence is a list of tuples.
+
+    Returns:
+        - Tuple[List[List[Tuple[str, str]]], List[List[Tuple[str, str]]]]: Two lists of sentences.
+    """
+    index = 0
+
+    # Calculate the index to split the sentences
+    for _, sentence in enumerate(sentences):
+        for _, token in enumerate(sentence):
+            if len(token) > 1:
+                index += 1
+            else:
+                break
+
+    # Split the sentences into two parts
+    sentences1 = []
+    sentences2 = []
+    for _, sentence in enumerate(sentences):
+        if index > 0:
+            sentences1.append(sentence)
+            index -= len(sentence)
+        else:
+            sentences2.append(sentence)
+    return sentences1, sentences2
+
+
+def saveFile(sentences: List[List[Tuple[str, str]]], filename: str) -> None:
+    """
+    Save the sentences to a file.
+
+    Args:
+        - sentences (List[List[Tuple[str, str]]]): A list of sentences, where each sentence is a list of tuples.
+        - filename (str): The name of the file to save the sentences.
+
+    Returns:
+        - None
+    """
+    with open(filename, "w") as f:
+        for sentence in sentences:
+            for token in sentence:
+                f.write("\t".join(token) + "\n")
+            f.write("\n")
+
+
 if __name__ == "__main__":
     currentDir = os.path.dirname(os.path.abspath(__file__))
 
@@ -48,3 +100,8 @@ if __name__ == "__main__":
     sentences = iob.parse_file(originalFile)
 
     printPercentages(sentences)
+
+    # Divide the sentences into two parts
+    withLabels, unlabeled = divideSentences(sentences)
+
+    saveFile(unlabeled, os.path.join(currentDir, "data", "unlabeled.csv"))
