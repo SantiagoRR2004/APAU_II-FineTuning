30dec3ab1ca56093efd53aa756f0656309f8d9a4
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-05-05 13:55:24 +0200

    Add labels with Spacy

--- labelPropagation.py
+++ labelPropagation.py
@@ -2,6 +2,7 @@ import os
 import predict_crf
 import main
 from typing import List, Tuple
+import spacy
 
 
 def labelingOptions(sentences: List[List[Tuple[str, str]]], nLabels: int) -> None:
@@ -24,6 +25,7 @@ def labelingOptions(sentences: List[List[Tuple[str, str]]], nLabels: int) -> Non
     os.makedirs(os.path.join(currentDir, "data", "labelOptions"), exist_ok=True)
 
     labelCRF(sentences, nLabels)
+    labelSpacy(sentences, nLabels)
 
 
 def labelCRF(sentences: List[List[Tuple[str, str]]], nLabels: int) -> None:
@@ -48,4 +50,45 @@ def labelCRF(sentences: List[List[Tuple[str, str]]], nLabels: int) -> None:
     main.saveFile(
         cfrSentences[nLabels:],
         os.path.join(currentDir, "data", "labelOptions", "cfr.csv"),
+    )
+
+
+def labelSpacy(sentences: List[List[Tuple[str, str]]], nLabels: int) -> None:
+    """
+    Use the Spacy model to predict labels for the unlabeled sentences and save them to a file.
+
+    Args:
+        - sentences (List[List[Tuple[str, str]]]): A list of sentences, where each sentence is a list of tuples.
+        - nLabels (int): The number of labeled sentences.
+
+    Returns:
+        - None
+    """
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+
+    model_name = "es_core_news_lg"
+
+    try:
+        nlp = spacy.load(model_name)
+    except OSError:
+        spacy.cli.download(model_name)
+        nlp = spacy.load(model_name)
+
+    labeledSentences = []
+
+    for sentence in sentences[-nLabels:]:
+        doc = nlp(" ".join([token[0] for token in sentence]))
+        labeledSentences.append(
+            [
+                (
+                    token.text,
+                    f"{token.ent_iob_}{'-' if token.ent_type_ else ''}{token.ent_type_}",
+                )
+                for token in doc
+            ]
+        )
+
+    # Save the unlabeled part
+    main.saveFile(
+        labeledSentences, os.path.join(currentDir, "data", "labelOptions", "spacy.csv")
     )
