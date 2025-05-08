0d1e549156454dad2c78b227026592f37d4706ab
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-05-08 19:13:45 +0200

    Add getConsensus function

--- labelPropagation.py
+++ labelPropagation.py
@@ -5,6 +5,8 @@ from typing import List, Tuple
 from spacy.tokens import Doc
 import spacy
 import tqdm
+import IOB
+import random
 
 
 def labelingOptions(sentences: List[List[Tuple[str, str]]], nLabels: int) -> None:
@@ -100,3 +102,53 @@ def labelSpacy(sentences: List[List[Tuple[str, str]]], nLabels: int) -> None:
     main.saveFile(
         labeledSentences, os.path.join(currentDir, "data", "labelOptions", "spacy.csv")
     )
+
+
+def getConsensus() -> List[List[Tuple[str, str]]]:
+    """
+    Get the consensus labels from the labeling options.
+    The function reads the labeling options from the labelOptions folder
+    and checks if all labels are the same.
+    If they are not it randomly chooses one of the labels.
+
+    Args:
+        - None
+
+    Returns:
+        - List[List[Tuple[str, str]]]: A list of sentences, where each sentence is a list of tuples.
+    """
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+
+    folderPath = os.path.join(currentDir, "data", "labelOptions")
+    files = {}
+
+    for file in os.listdir(folderPath):
+        if file.endswith(".csv"):
+            iob = IOB.IOB()
+
+            files[file[:-4]] = iob.parse_file(os.path.join(folderPath, file))
+
+    emptySentences = [[(t[0], None) for t in s] for s in list(files.values())[0]]
+
+    nAccordance = 0
+    nTokens = sum([len(sentence) for sentence in emptySentences])
+
+    for i in range(len(emptySentences)):
+        for j in range(len(emptySentences[i])):
+
+            tokenOptions = [f[i][j][1] for f in files.values()]
+
+            # Check if all labels are the same
+            if len(set(tokenOptions)) == 1:
+                emptySentences[i][j] = (emptySentences[i][j][0], tokenOptions[0])
+                nAccordance += 1
+            else:
+                # If not, we randomly choose one of the labels
+                emptySentences[i][j] = (
+                    emptySentences[i][j][0],
+                    random.choice(tokenOptions),
+                )
+
+    print(f"Percentage of unanimous labels: {nAccordance / nTokens * 100:.2f}%")
+
+    return emptySentences
